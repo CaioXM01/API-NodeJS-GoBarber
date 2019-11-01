b8e3efc09bc871c6f7bf1f44b0333f56980b6360
CaioXM01 <caioxm01@gmail.com>
2019-11-01 01:19:16 -0300

    Configurando Nodemailer e templates de e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,20 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'aa9afc88ee9abc',
+    pass: '4603be9775159f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
+
+/**
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp)
+ * Gmail
+ */
